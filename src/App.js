d47e68d15606d1bd0ad7ebba60de529f1d0567d9
ShrutiChouhan123 <shchouhan@bestpeers.in>
2023-10-25 16:31:50 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,65 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css';
 import Product from "./Components/ProductData";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Cart from "./Components/Cart";
-import Wishlist from "./Components/Wishlist"; 
+import Wishlist from "./Components/Wishlist";
 import { BrowserRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './index.css';
 
 
 
 function App() {
-   const cart = useSelector(state=>state.cart.cart);
-   const fev = useSelector(state=>state.fev.fev)
+  const cart = useSelector(state => state.cart.cart);
+  const fev = useSelector(state => state.fev.fev)
 
   return (
     <BrowserRouter>
 
-    <>
-  
-      <div className="body-navbar">
-        <ul className="nav-links">
-          <li>
-            <Link to='/' className="nav-buttons">Home</Link>
-          </li>
-          <li className="center">
-            <Link to='/products' className="nav-buttons">Products</Link>
-          </li>
-          <li className="upward" style={{ marginLeft: "40rem" }}>
-            <Link to='/cart' className="nav-buttons">
-            <span><span class="material-icons">shopping_cart </span>{cart.length}</span>  
-            
-            </Link>
-          </li>
-          <li className="forward" >
-            <Link to='/wishlist' className="nav-buttons">
-             <span><i class="material-icons">favorite</i>{fev.length}</span> 
-            
-            </Link>
-          </li>
-        </ul>
+      <>
 
-     
-      </div>
+        <div className="body-navbar">
+          <ul className="nav-links">
+            <li>
+              <Link to='/' className="nav-buttons">Home</Link>
+            </li>
+            <li className="center">
+              <Link to='/products' className="nav-buttons">Products</Link>
+            </li>
+            <li className="upward" style={{ marginLeft: "40rem" }} >
+              <Link to='/cart' className="nav-buttons">
+                <span class="material-icons" >shopping_cart </span>
+                <span class="notification-counter">{cart.length}</span>
 
-     
-      <Routes>
-        <Route exact path="/" element={<Home />} ></Route>
-        <Route exact path="/products" element={<Product/>}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/wishlist" element={<Wishlist />}></Route>
-      </Routes>
+              </Link>
+
+            </li>
+            <li className="forward" >
+              <Link to='/wishlist' className="nav-buttons">
+              <span class="material-icons" > <i class="material-icons">favorite</i></span>
+              <span class="notification-counter">{fev.length}</span>
+              
+
+              </Link>
+            </li>
+          </ul>
 
 
-    </>
+        </div>
+
+
+        <Routes>
+          <Route exact path="/" element={<Home />} ></Route>
+          <Route exact path="/products" element={<Product />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/wishlist" element={<Wishlist />}></Route>
+        </Routes>
+
+
+      </>
     </BrowserRouter>
 
   );
